4a3f046e4bf50e6aaf5eef532f618b5ca9f01a80
St3nzz <38786257+St3nzz@users.noreply.github.com>
2021-08-15 22:29:53 +0400

    3 comm
    3 comm

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,9 @@ input.onfocus = () => {
 input.onblur = () => {
     input.classList.remove('active');
 }
+
+top__mail = document.getElementById('top__mail');
+document.documentElement.clientWidth <= 974 ? top__mail.style.display = "none" : top__mail.style.display = "block";
+window.addEventListener('resize',function(){    
+    document.documentElement.clientWidth <= 974 ? top__mail.style.display = "none" : top__mail.style.display = "block";
+});
